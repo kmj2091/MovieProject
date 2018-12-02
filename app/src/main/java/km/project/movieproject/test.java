f1269613a471f65de79daa06d0941341d51edb53
mjkim <kmj2091@naver.com>
2018-12-02 17:27:46 +0900

    "Seond Commit"

---
+++ app/src/main/java/km/project/movieproject/test.java
@@ -0,0 +1,20 @@
+package km.project.movieproject;
+
+import android.app.Activity;
+
+public class test {
+
+    private static final String TAG = test.class.getSimpleName();
+
+    public test(Activity activity){
+
+        onCreate();
+    }
+
+    private void onCreate(){
+
+
+
+    }
+
+}
